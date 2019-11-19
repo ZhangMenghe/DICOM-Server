b49e3a76e5b26714d1cc744c44631515841057cf
ZhangMenghe <zmh.leaves@gmail.com>
2019-11-18 16:20:40 -0800

    dicom 1 byte

--- dicomUtils.py
+++ dicomUtils.py
@@ -1,12 +1,13 @@
 from os import listdir,path
 import pydicom
+import numpy as np
 from pydicom.data import get_testdata_files
 from transManager_pb2 import dcmImage, bundleConfig
 
 def processDICOM(file_name):
     ds = pydicom.dcmread(file_name)
     print("read dicom : " + file_name)
-    return dcmImage(dcmID = 0, position = float(ds[0x0020,0x1041].value), data=ds.pixel_array.tobytes())
+    return dcmImage(dcmID = 0, position = float(ds[0x0020,0x1041].value), data=ds.pixel_array.astype(np.ubyte).tobytes())
 
 def getBundleConfig(folder_name):
     flist = listdir(folder_name)
