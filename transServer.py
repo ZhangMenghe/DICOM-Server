6044888be30288bf5a85598bd456f88ce40fe2af
ZhangMenghe <zmh.leaves@gmail.com>
2019-11-26 13:04:19 -0800

    save states

--- transServer.py
+++ transServer.py
@@ -14,28 +14,36 @@ class transServer(dataTransferServicer):
     def __init__(self):
         class Servicer(dataTransferServicer):
             def __init__(self):
-                self.request_path = None
+                self.request_folder = None
+                self.local_data_path = "data"
+                self.local_mask_path = "data_mask"
+                self.trans_manager = transDataManager()
             def getAvailableDatasetInfos(self, request, context):
                 print("===Request all avaliable dataset of remote server===")
                 return get_all_available_datasets(REMOTE_ADDR)
 
             def getConfig(self, request, context):
                 print("Request images in folder: " + request.req_msg)
-                if not check_or_download_from_outside_server(REMOTE_ADDR, request.req_msg):
+                if not check_or_download_from_outside_server(REMOTE_ADDR, self.local_data_path, request.req_msg):
                     print("ERROR: Requested File Not exist")
                     return bundleConfig(file_nums = 0)
-                self.request_path = request.req_msg    
-                return parser_folder_config(request.req_msg)
+                self.request_folder = request.req_msg
+                print("=====???")  
+                return parser_folder_config(self.local_data_path, request.req_msg)
 
             def Download(self, request, context):
-                print("Download from .." + self.request_path)
-                return download_folder_as_stream(self.request_path)
+                print("Download from .." + self.request_folder)
+                return self.trans_manager.download_folder_as_stream(self.local_data_path, self.request_folder)
+                # return download_folder_as_stream(self.local_data_path, self.request_folder)
+            def getMasks(self, request, context):
+                print("Trying to return or inference Segmentation...")
+                return inference_masks_as_stream(self.local_mask_path, self.request_folder)
 
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         add_dataTransferServicer_to_server(Servicer(), self.server)
     
     def run(self, port):
-        print("------------------start Python GRPC server")
+        print("-----------start Python GRPC server-----------")
         self.server.add_insecure_port(port)
         self.server.start()
         self.server.wait_for_termination()
