dc1f6bd784c5d7471ce68796d5f8dab950023d60
ZhangMenghe <zmh.leaves@gmail.com>
2019-11-26 13:31:12 -0800

    re-orgnize the trans utils

--- transServer.py
+++ transServer.py
@@ -14,30 +14,29 @@ class transServer(dataTransferServicer):
     def __init__(self):
         class Servicer(dataTransferServicer):
             def __init__(self):
-                self.request_folder = None
                 self.local_data_path = "data"
                 self.local_mask_path = "data_mask"
-                self.trans_manager = transDataManager()
+                self.trans_manager = None
             def getAvailableDatasetInfos(self, request, context):
                 print("===Request all avaliable dataset of remote server===")
                 return get_all_available_datasets(REMOTE_ADDR)
 
             def getConfig(self, request, context):
                 print("Request images in folder: " + request.req_msg)
-                if not check_or_download_from_outside_server(REMOTE_ADDR, self.local_data_path, request.req_msg):
+                self.trans_manager = transDataManager(self.local_data_path, request.req_msg)
+                self.trans_manager.check_or_download_from_outside_server(REMOTE_ADDR)
+                if not self.trans_manager.check_or_download_from_outside_server(REMOTE_ADDR):
                     print("ERROR: Requested File Not exist")
                     return bundleConfig(file_nums = 0)
-                self.request_folder = request.req_msg
-                print("=====???")  
-                return parser_folder_config(self.local_data_path, request.req_msg)
+                print("=====")
+                return self.trans_manager.parser_folder_config()
 
             def Download(self, request, context):
-                print("Download from .." + self.request_folder)
-                return self.trans_manager.download_folder_as_stream(self.local_data_path, self.request_folder)
-                # return download_folder_as_stream(self.local_data_path, self.request_folder)
+                print("Download from ..")
+                return self.trans_manager.download_folder_as_stream()
             def getMasks(self, request, context):
                 print("Trying to return or inference Segmentation...")
-                return inference_masks_as_stream(self.local_mask_path, self.request_folder)
+                return inference_masks_as_stream()
 
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         add_dataTransferServicer_to_server(Servicer(), self.server)
--- transUtils.py
+++ transUtils.py
@@ -5,49 +5,11 @@ from dicomUtils import *
     
 def get_all_available_datasets(remote_path):
     if not path.isdir(remote_path):
-        print("not a dir : "+ remote_path)
+        print("not a dir remote : "+ remote_path)
         return
     #todo: parser sth like .csv to get info
     for folder_name in listdir(remote_path):
         yield datasetInfo(folder_name = folder_name,patient_name="Larry Smarr", date="01/01/19", file_nums=48)    
-
-        
-def check_or_download_from_outside_server(remote_path, local_path, folder_name):
-    local_folder_path = path.join(getcwd(), local_path, folder_name)
-    remote_folder_path = path.join(remote_path, folder_name)
-    if(path.isdir(local_folder_path)):
-        print("====local exist")
-        return True
-    if not path.isdir(remote_folder_path):
-        print("not a dir : "+ remote_folder_path)
-        return False
-    # copy from remote_path
-    destination = shutil.copytree(remote_folder_path, local_folder_path)
-    print("finish copying to " + destination)
-    return True
-
-def parser_folder_config(local_path, folder_name):
-    local_folder_path = path.join(getcwd(), local_path, folder_name)
-    if not path.isdir(local_folder_path):
-        print(folder_name + " : Not a path, please double check the name")
-        return bundleConfig(file_nums = 0)
-    print("folder path: " + folder_name)
-    return getBundleConfig(local_folder_path)
-
-def download_folder_as_stream(local_path, folder_name):
-    dcm_list=[]
-    path_pre=path.join(getcwd(), local_path, folder_name)
-    print(path_pre)
-    for file_path in listdir(path_pre):
-        print("file " + file_path)
-        dcm_list.append(processDICOM(path.join(path_pre, file_path)))
-        dcm_list.sort(key=lambda x: x.position, reverse=False)
-    for i in range(len(dcm_list)):
-        dcm_list[i].dcmID = i
-        yield dcm_list[i]
-
-# def inference_masks_as_stream(local_path, folder_name):
-
 
 def save_dcmImgs_to_file(dcm_iterators, out_folder):
     if not path.exists(out_folder):
@@ -58,18 +20,40 @@ def save_dcmImgs_to_file(dcm_iterators, out_folder):
         print("write to file id %f, " % dcm.position)
 
 class transDataManager():
-    def __init__(self):
+    def __init__(self, local_data_path, folder_name):
+        self.rq_foldername = folder_name
+        self.rq_folder_local_path = path.join(local_data_path, folder_name)
+        self.rq_folder_abs_path = path.join(getcwd(), local_data_path, folder_name) 
         self.dcm_list = []
-    def download_folder_as_stream(self, local_path, folder_name):
+    def check_or_download_from_outside_server(self, remote_path):
         print("=====")
+        remote_folder_path = path.join(remote_path, self.rq_foldername)
+        if(path.isdir(self.rq_folder_local_path)):
+            print("===return")
+            return True
+        if not path.isdir(remote_folder_path):
+            print("not a dir in remote: "+ remote_folder_path)
+            return False
+        # copy from remote_path
+        destination = shutil.copytree(remote_folder_path, self.rq_folder_local_path)
+        print("finish copying to " + destination)
+        return True
+    def parser_folder_config(self):
+        if not path.isdir(self.rq_folder_local_path):
+            print(self.rq_folder_local_path + " : Not a path, please double check the name")
+            return bundleConfig(file_nums = 0)
+        print("folder path: " + self.rq_folder_local_path)
+        return getBundleConfig(self.rq_folder_abs_path)
+        
+    def download_folder_as_stream(self):
         self.dcm_list.clear()
-        path_pre=path.join(getcwd(), local_path, folder_name)
-        for file_path in listdir(path_pre):
-            self.dcm_list.append(processDICOM(path.join(path_pre, file_path)))
+        for file_path in listdir(self.rq_folder_local_path):
+            self.dcm_list.append(processDICOM(path.join(self.rq_folder_local_path, file_path)))
             self.dcm_list.sort(key=lambda x: x.position, reverse=False)
         for i in range(len(self.dcm_list)):
             self.dcm_list[i].dcmID = i
             yield self.dcm_list[i]
+
     def inference_masks_as_stream(self):
         for i in range(len(self.dcm_list)):
             print("======inference  %d" % self.dcm_list[i].position)
